26099f68955ed23b664f39aa8bd29931d5a92fff
anjana <anjanaanjuz494@gmail.com>
2022-05-10 14:47:17 +0530

    Sum Of Two Numbers

---
+++ sum_of_two_no.py
@@ -0,0 +1,8 @@
+#This program adds two numbers
+
+def sum(a, b):      # defining sum function
+    c = a + b           # Add two numbers
+    print(a, "+", b, "=", c)        # displaying sum
+
+
+sum(1, 2)
